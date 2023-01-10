1a2312d0cafc1f0d004d5636121fd6eba227a440
Roberto Martins <roberto.martins.info@gmail.com>
2023-01-10 14:17:37 -0300

    kafka producer update

--- producer_kafka/producer.rb
+++ producer_kafka/producer.rb
@@ -1,11 +1,18 @@
 require 'kafka'
 require 'time'
+require 'faker'
 
 kafka = Kafka.new(['kafka:9092'], client_id: 'example-aplication')
 
 (1..).each do |i|
-  kafka.deliver_message("#{Time.now}", topic: 'example_test_topic')
+  kafka.deliver_message({ time: Time.now,
+                          author: Faker::Name.name,
+                          message: Faker::Lorem.sentence,
+                          kind: 'post'
+                        }.to_json, topic: 'example_test_topic')
+
   p "Message #{Time.now.utc.strftime("%M:%S")}"
+
   sleep 3
 end
 
